ea241c09bb1028b96ab55a6f710d529b3a5c2124
root@10.1.30.2 <root@10.1.30.2>
2020-04-07 03:58:54 +0000

    fix deploy shell

--- deploy-bootstrap.sh
+++ deploy-bootstrap.sh
@@ -3,12 +3,9 @@
 
 # nohup ./scripts/init-network.sh >>boostrap.log 2>&1 & # do this at first
 
-echo "./lotus --repo=/data/lotus/dev/.ldt0111 wallet balance"
-./lotus --repo=/data/lotus/dev/.ldt0111 wallet balance
-
 ssh-copy-id root@127.0.0.1
 ./scripts/setup-host.sh root@127.0.0.1
 ./scripts/deploy-node.sh root@127.0.0.1
 ./scripts/deploy-bootstrapper.sh root@127.0.0.1
 sleep 10
-# nohup ./fountain run --front=0.0.0.0:7777 --from=$(lotus wallet default) >fountain.log 2>&1 &
+nohup ./fountain run --front=0.0.0.0:7777 --from=$(lotus wallet default) >fountain.log 2>&1 &
