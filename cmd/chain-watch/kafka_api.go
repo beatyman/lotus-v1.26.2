66afb66122d0170c0d325052ee55ecdcb37f8d35
root@HLM-NjY4NTIwOTI3Cg <root@HLM-NjY4NTIwOTI3Cg>
2020-04-28 13:00:58 +0800

    bak

--- cmd/chain-watch/kafka_api.go
+++ cmd/chain-watch/kafka_api.go
@@ -28,6 +28,7 @@ func CloseKafkaProducer() {
 	if _kp != nil {
 		_kp.Close()
 	}
+	time.Sleep(3e9)
 }
 
 func GetKafkaProducer() (sarama.SyncProducer, error) {
@@ -74,24 +75,27 @@ func GetKafkaProducer() (sarama.SyncProducer, error) {
 func KafkaProducer(producerData string, topic string) error {
 	// TODO: fix this to pool
 	go func(m string) {
+	retry:
 		p, err := GetKafkaProducer()
 		if err != nil {
 			log.Error(err)
 			CloseKafkaProducer()
-			return
+			goto retry
 		}
 		msg := &sarama.ProducerMessage{
 			Topic: topic,
 			Value: sarama.ByteEncoder(producerData),
 		}
 		log.Infof("send kafka msg:%s", m)
-		part, offset, err := p.SendMessage(msg)
-		if err != nil {
-			log.Error(err)
-			CloseKafkaProducer()
-			return
-		}
-		log.Infof("send kafka msg done:%s, partition:%d,offset:%d", m, part, offset)
+		_ = p
+		_ = msg
+		// part, offset, err := p.SendMessage(msg)
+		// if err != nil {
+		// 	log.Error(err)
+		// 	CloseKafkaProducer()
+		// 	goto retry
+		// }
+		// log.Infof("send kafka msg done:%s, partition:%d,offset:%d", m, part, offset)
 	}(producerData)
 
 	return nil
