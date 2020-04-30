d81eb4398c9e4c359f2b4e71b4eb774922012c75
root@10.1.30.2 <root@10.1.30.2>
2020-04-30 11:53:48 +0000

    fix message sub

--- cmd/chain-watch/sync.go
+++ cmd/chain-watch/sync.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/store"
-	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func runSyncer(ctx context.Context, api api.FullNode, st io.Writer, maxBatch int) {
@@ -21,23 +20,16 @@ func runSyncer(ctx context.Context, api api.FullNode, st io.Writer, maxBatch int
 				case store.HCCurrent:
 					fallthrough
 				case store.HCApply:
-					go syncHead(ctx, api, st, change.Val, maxBatch)
+					syncHead(ctx, api, st, change.Val, maxBatch)
 				case store.HCRevert:
 					log.Warnf("revert todo")
 				}
 				log.Info("=====message======", change.Type, ":", store.HCCurrent)
 				if change.Type == store.HCCurrent {
-					go subMpool(ctx, api, st, change.Val)
 					// go subMpool(ctx, api, st, change.Val)
 					// go subBlocks(ctx, api, st)
-					// go subInfo(ctx, api, st, change.Val)
 				}
 			}
 		}
 	}()
-}
-
-func subInfo(ctx context.Context, api api.FullNode, st io.Writer, ts *types.TipSet) {
-	subMpool(ctx, api, st, ts)
-	subBlocks(ctx, api, st)
 }
