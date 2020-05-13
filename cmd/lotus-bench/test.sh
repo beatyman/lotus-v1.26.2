419cf3cad4e1b222995a567cc593736c90e6111b
root@10.40.50.3 <root@10.40.50.3>
2020-05-13 00:58:06 +0000

    add bench.sh

--- cmd/lotus-bench/test.sh
+++ cmd/lotus-bench/test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-#size=536870912
-size=34359738368 # 32GB
-FIL_PROOFS_MAXIMIZE_CACHING=1 ./lotus-bench sealing --storage-dir=/data/cache/.lotus-bench --sector-size=$size
+size=536870912
+RUST_LOG=info RUST_BACKTRACE=1 TMPDIR=/data/cache/tmp ./lotus-bench sealing --storage-dir=/data/cache/.lotus-bench --sector-size=$size --no-gpu
+
+#size=34359738368 # 32GB
+#FIL_PROOFS_MAXIMIZE_CACHING=1 ./lotus-bench sealing --storage-dir=/data/cache/.lotus-bench --sector-size=$size --no-gpu
