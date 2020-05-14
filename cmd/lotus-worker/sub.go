d6f0a94b2c87887bbeec061193c4ba3dbe45dc77
root@DN-008-012 <root@DN-008-012>
2020-05-14 21:35:19 +0000

    loop any error for work fetch

--- cmd/lotus-worker/sub.go
+++ cmd/lotus-worker/sub.go
@@ -388,16 +388,16 @@ func (w *worker) processTask(ctx context.Context, task ffiwrapper.WorkerTask) ff
 			task.SectorStorage.SectorInfo.ID,
 			task.Type,
 		); err != nil {
-			if errors.ErrNoData.Equal(err) {
-				// release bandwidth
-				if err := api.WorkerAddConn(ctx, task.WorkerID, -1); err != nil {
-					ReleaseNodeApi(false)
-					return errRes(errors.As(err, w.workerCfg), task)
-				}
-
-				// return the err task not found and drop it.
-				return errRes(ffiwrapper.ErrTaskNotFound.As(err, task), task)
-			}
+			//if errors.ErrNoData.Equal(err) {
+			//	// release bandwidth
+			//	if err := api.WorkerAddConn(ctx, task.WorkerID, -1); err != nil {
+			//		ReleaseNodeApi(false)
+			//		return errRes(errors.As(err, w.workerCfg), task)
+			//	}
+			//
+			//	// return the err task not found and drop it.
+			//	return errRes(ffiwrapper.ErrTaskNotFound.As(err, task), task)
+			//}
 			log.Warnf("fileserver error, retry 10s later:%+s", err.Error())
 			time.Sleep(10e9)
 			goto retryFetch
