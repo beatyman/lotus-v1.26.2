d87255dc7b7e517149f68283fbd186d195d9c699
root@10.40.22.18 <root@10.40.22.18>
2020-06-09 23:11:42 +0000

    fix bench for gpu

--- bench.sh
+++ bench.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
-# REAME
-# make bench
-# nohup ./bench.sh &
-# tail -f nohup.out
-# REAME end
-
 export IPFS_GATEWAY="https://proof-parameters.s3.cn-south-1.jdcloud-oss.com/ipfs/"
 
-export FIL_PROOFS_USE_GPU_COLUMN_BUILDER=1 # give us gpu precommit2, https://filecoinproject.slack.com/archives/CEGB67XJ8/p1588805545137700
-export FIL_PROOFS_MAXIMIZE_CACHING=0  # open cache for 32GB or 64GB
+# Note that FIL_PROOFS_USE_GPU_TREE_BUILDER=1 is for tree_r_last building and FIL_PROOFS_USE_GPU_COLUMN_BUILDER=1 is for tree_c.  
+# So be sure to use both if you want both built on the GPU
+export FIL_PROOFS_USE_GPU_COLUMN_BUILDER=1
+export FIL_PROOFS_USE_GPU_TREE_BUILDER=1 
+export FIL_PROOFS_MAXIMIZE_CACHING=1  # open cache for 32GB or 64GB
 
-size=536870912
+#size=536870912
 #size=2048
-RUST_LOG=info RUST_BACKTRACE=1 ./bench sealing --storage-dir=/data/cache/.lotus-bench --sector-size=$size --skip-unseal=true --parallel=1
-#RUST_LOG=info RUST_BACKTRACE=1 ./bench sealing --storage-dir=/data/cache/.lotus-bench --sector-size=$size --parallel=1
+#RUST_LOG=info RUST_BACKTRACE=1 ./bench sealing --storage-dir=/data/cache/.lotus-bench --sector-size=$size --skip-unseal=true #--parallel=2
+##RUST_LOG=info RUST_BACKTRACE=1 ./bench sealing --storage-dir=/data/cache/.lotus-bench --sector-size=$size #--parallel=1
 
-#size=34359738368 # 32GB
-#RUST_LOG=info RUST_BACKTRACE=1 ./bench sealing --storage-dir=/data/cache/.lotus-bench --sector-size=$size
+size=34359738368 # 32GB
+RUST_LOG=info RUST_BACKTRACE=1 ./bench sealing --storage-dir=/data/cache/.lotus-bench --sector-size=$size --skip-unseal=true
