06d808e346e174bcd09344ea0a3b38239e87e6bd
root@CN-000003 <root@CN-000003>
2020-06-09 09:23:04 +0000

    fix bench

--- bench.sh
+++ bench.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# REAME
+# make bench
+# nohup ./bench.sh &
+# tail -f nohup.out
+# REAME end
+
 export IPFS_GATEWAY="https://proof-parameters.s3.cn-south-1.jdcloud-oss.com/ipfs/"
 
 export FIL_PROOFS_USE_GPU_COLUMN_BUILDER=1 # give us gpu precommit2, https://filecoinproject.slack.com/archives/CEGB67XJ8/p1588805545137700
@@ -11,4 +17,4 @@ RUST_LOG=info RUST_BACKTRACE=1 ./bench sealing --storage-dir=/data/cache/.lotus-
 #RUST_LOG=info RUST_BACKTRACE=1 ./bench sealing --storage-dir=/data/cache/.lotus-bench --sector-size=$size --parallel=1
 
 #size=34359738368 # 32GB
-#FIL_PROOFS_MAXIMIZE_CACHING=1 RUST_LOG=info RUST_BACKTRACE=1 ./bench sealing --storage-dir=/data/cache/.lotus-bench --sector-size=$size 
+#RUST_LOG=info RUST_BACKTRACE=1 ./bench sealing --storage-dir=/data/cache/.lotus-bench --sector-size=$size
