a434ad7a3fffa3701d74d2b489c86531c7bac809
zhoushuyue <zhoushuyue@grandhelmsman.com>
2020-07-26 13:07:39 +0000

    fix publish

--- publish.sh
+++ publish.sh
@@ -2,4 +2,21 @@
 
 export FILECOIN_BIN=./deploy/lotus
 
-./install.sh $@
+ver=""
+if [ ! -z "$1" ]; then
+    ver=$1"-"
+fi
+ver=$ver$(git describe --always --match=NeVeRmAtCh --dirty)
+
+rm -rf ./deploy
+git checkout deploy
+
+./install.sh
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+cd ./deploy
+tar -czf lotus-$ver.tar.gz lotus
+
