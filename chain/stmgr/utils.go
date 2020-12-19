89f14fec598c660be0b1565454da51719f25b03c
zhoushuyue <zhouhsuyue@grandhelmsman.com>
2020-12-20 05:57:30 +0800

    fix log

--- chain/stmgr/utils.go
+++ chain/stmgr/utils.go
@@ -476,7 +476,6 @@ func MinerGetBaseInfo(ctx context.Context, sm *StateManager, bcs beacon.Schedule
 
 	act, err := sm.LoadActorRaw(ctx, maddr, lbst)
 	if xerrors.Is(err, types.ErrActorNotFound) {
-		log.Error(err)
 		_, err := sm.LoadActor(ctx, maddr, ts)
 		if err != nil {
 			return nil, xerrors.Errorf("loading miner in current state: %w", err)
@@ -511,7 +510,6 @@ func MinerGetBaseInfo(ctx context.Context, sm *StateManager, bcs beacon.Schedule
 	}
 
 	if len(sectors) == 0 {
-		log.Error("No sector for winning post")
 		return nil, nil
 	}
 
