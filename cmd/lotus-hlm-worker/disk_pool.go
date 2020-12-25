8bfc64081f888171d593b3a7284caa9776e9e6b3
zhoushuyue <zhouhsuyue@grandhelmsman.com>
2020-12-25 19:48:50 +0800

    add disk pool interface

---
+++ cmd/lotus-hlm-worker/disk_pool.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/gwaylib/errors"
+
+type DiskPool interface {
+	// allocate disk and return repo
+	//
+	// return
+	// repo -- like /data/lotus-cache/1
+	Allocate(sid string) (string, error)
+
+	// delete disk data with sector id
+	Delete(sid string) error
+}
+
+const (
+	DISK_MOUNT_ROOT = "/data/lotus-cache/"
+)
+
+func NewDiskPoll() (DiskPool, error) {
+	return nil, errors.New("TODO")
+}
