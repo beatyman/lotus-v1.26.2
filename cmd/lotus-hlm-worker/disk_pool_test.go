d721969e394c8e31e58bb871a25ac5339e0f4329
zhoushuyue <zhouhsuyue@grandhelmsman.com>
2020-12-29 14:52:30 +0800

    fix test case

--- cmd/lotus-hlm-worker/disk_pool_test.go
+++ cmd/lotus-hlm-worker/disk_pool_test.go
@@ -36,6 +36,8 @@ func TestMain(m *testing.M) {
 }
 
 func TestDiskPoolAllocate(t *testing.T) {
+	// diskNum := 4
+	// taskPerDisk = 2
 	tasks := []string{
 		"s-t01000-0",
 		"s-t01000-1",
@@ -45,28 +47,6 @@ func TestDiskPoolAllocate(t *testing.T) {
 		"s-t01000-5",
 		"s-t01000-6",
 		"s-t01000-7",
-		"s-t01000-8",
-		"s-t01000-9",
-		"s-t01000-10",
-		"s-t01000-11",
-		"s-t01000-12",
-		"s-t01000-13",
-		"s-t01000-14",
-		"s-t01000-15",
-		"s-t01000-16",
-		"s-t01000-17",
-		"s-t01000-18",
-		"s-t01000-19",
-		"s-t01000-20",
-		"s-t01000-21",
-		"s-t01000-22",
-		"s-t01000-23",
-		"s-t01000-24",
-		"s-t01000-25",
-		"s-t01000-26",
-		"s-t01000-27",
-		"s-t01000-28",
-		"s-t01000-29",
 	}
 
 	pool, err := NewDiskPool()
@@ -91,6 +71,11 @@ func TestDiskPoolAllocate(t *testing.T) {
 		}
 	}
 	// TODO: checksum disk stat.
+
+	// full allocate
+	if _, err := pool.Allocate("s-t01000-8"); err == nil {
+		t.Fatal("expect no disk to allocate")
+	}
 
 	// simulate restart process.
 	rpool, err := NewDiskPool()
@@ -124,28 +109,6 @@ func TestDiskPoolAllocate(t *testing.T) {
 		"s-t01000-105",
 		"s-t01000-106",
 		"s-t01000-107",
-		"s-t01000-108",
-		"s-t01000-109",
-		"s-t01000-110",
-		"s-t01000-111",
-		"s-t01000-112",
-		"s-t01000-113",
-		"s-t01000-114",
-		"s-t01000-115",
-		"s-t01000-116",
-		"s-t01000-117",
-		"s-t01000-118",
-		"s-t01000-119",
-		"s-t01000-120",
-		"s-t01000-121",
-		"s-t01000-122",
-		"s-t01000-123",
-		"s-t01000-124",
-		"s-t01000-125",
-		"s-t01000-126",
-		"s-t01000-127",
-		"s-t01000-128",
-		"s-t01000-129",
 	}
 	for idx, task := range tasks {
 		if _, err := pool.Allocate(task); err != nil {
@@ -169,28 +132,6 @@ func TestDiskPoolAllocate(t *testing.T) {
 		"s-t01001-105",
 		"s-t01001-106",
 		"s-t01001-107",
-		"s-t01001-108",
-		"s-t01001-109",
-		"s-t01001-110",
-		"s-t01001-111",
-		"s-t01001-112",
-		"s-t01001-113",
-		"s-t01001-114",
-		"s-t01001-115",
-		"s-t01001-116",
-		"s-t01001-117",
-		"s-t01001-118",
-		"s-t01001-119",
-		"s-t01001-120",
-		"s-t01001-121",
-		"s-t01001-122",
-		"s-t01001-123",
-		"s-t01001-124",
-		"s-t01001-125",
-		"s-t01001-126",
-		"s-t01001-127",
-		"s-t01001-128",
-		"s-t01001-129",
 	}
 	for idx, task := range tasks {
 		if _, err := rpool.Allocate(task); err != nil {
