f0846a977d8061956f13ccca8ee5f29f9f22bb81
唐顺才 <tangshuncai@grandhelmsman.com>
2020-12-29 10:24:07 +0800

    加入worker cache盘，扇区->ssd映射调度

--- cmd/lotus-hlm-worker/disk_pool.go
+++ cmd/lotus-hlm-worker/disk_pool.go
@@ -1,6 +1,20 @@
 package main
 
-import "github.com/gwaylib/errors"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"syscall"
+
+	"github.com/gwaylib/errors"
+)
+
+const (
+	//DISK_MOUNT_ROOT = "/data/lotus-cache/"
+	DISK_MOUNT_ROOT = "/run/user/"
+)
 
 type DiskPool interface {
 	// allocate disk and return repo
@@ -9,14 +23,208 @@ type DiskPool interface {
 	// repo -- like /data/lotus-cache/1
 	Allocate(sid string) (string, error)
 
+	// query sid's cache directory with sector id
+	Query(sid string) (string, error)
+
 	// delete disk data with sector id
 	Delete(sid string) error
+
+	Show() error
+}
+
+func NewDiskPool() (DiskPool, error) {
+	dp := &SSM{}
+	dp.Init()
+
+	return dp, nil
 }
 
 const (
-	DISK_MOUNT_ROOT = "/data/lotus-cache/"
+	B  = 1
+	KB = 1024 * B
+	MB = 1024 * KB
+	GB = 1024 * MB
+	TB = 1024 * GB
+	PB = 1024 * TB
+	EB = 1024 * PB
+	ZB = 1024 * EB
+	YB = 1024 * ZB
 )
 
-func NewDiskPool() (DiskPool, error) {
-	return nil, errors.New("TODO")
+type DiskStatus struct {
+	All        uint64 `json:"all"`
+	Used       uint64 `json:"used"`
+	Free       uint64 `json:"free"`
+	MaxSector  uint64 `json:"maxsector"`
+	UsedSector uint64 `json:"usedsector"`
+}
+
+func sectorallot(disk *DiskStatus) {
+	disk.MaxSector = 100
+	disk.UsedSector = 0
+}
+
+// disk usage of path/disk
+func DiskUsage(path string) (disk DiskStatus) {
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs(path, &fs)
+	if err != nil {
+		panic(err)
+		return
+	}
+	disk.All = fs.Blocks * uint64(fs.Bsize)
+	disk.Free = fs.Bfree * uint64(fs.Bsize)
+	disk.Used = disk.All - disk.Free
+	sectorallot(&disk)
+
+	return
+}
+
+// 字节的单位转换 保留两位小数
+func formatFileSize(fileSize uint64) (size string) {
+	if fileSize < KB {
+		return fmt.Sprintf("%.2fB", float64(fileSize)/float64(B))
+	} else if fileSize < MB {
+		return fmt.Sprintf("%.2fKB", float64(fileSize)/float64(KB))
+	} else if fileSize < GB {
+		return fmt.Sprintf("%.2fMB", float64(fileSize)/float64(MB))
+	} else if fileSize < TB {
+		return fmt.Sprintf("%.2fGB", float64(fileSize)/float64(GB))
+	} else if fileSize < PB {
+		return fmt.Sprintf("%.2fTB", float64(fileSize)/float64(TB))
+	} else if fileSize < EB {
+		return fmt.Sprintf("%.2fPB", float64(fileSize)/float64(PB))
+	} else {
+		return fmt.Sprintf("%.2fEB", float64(fileSize)/float64(EB))
+	}
+}
+
+func exeSysCommand(cmdStr string) ([]byte, error) {
+	cmd := exec.Command("sh", "-c", cmdStr)
+	return cmd.Output()
+}
+
+func IsMountPoint(dir string) bool {
+	_, err := exeSysCommand("cat /proc/mounts | grep " + dir)
+	if err == nil {
+		return true
+	}
+	fmt.Println(dir, " is not mount point")
+
+	return false
+}
+
+type diskinfo struct {
+	mnt string
+	ds  DiskStatus
+}
+
+type SSM struct {
+	cursor int
+	ssize  uint64
+	captbl []*diskinfo
+	regtbl map[string]string    // sid:moint_point
+	maptbl map[string]*[]string // mount_point:sids
+}
+
+func (Ssm *SSM) Init() {
+	if _, err := os.Stat(DISK_MOUNT_ROOT); os.IsNotExist(err) {
+		//panic(err)
+		os.Mkdir(DISK_MOUNT_ROOT, os.ModePerm)
+	}
+
+	dir, err := ioutil.ReadDir(DISK_MOUNT_ROOT)
+	if err != nil {
+		panic(err)
+	}
+
+	Ssm.regtbl = make(map[string]string)
+	Ssm.maptbl = make(map[string]*[]string)
+	for _, d := range dir {
+		if d.IsDir() {
+			if IsMountPoint(path.Join(DISK_MOUNT_ROOT, d.Name())) == true {
+				Ssm.maptbl[path.Join(DISK_MOUNT_ROOT, d.Name())] = &[]string{}
+				Ssm.captbl = append(Ssm.captbl, &diskinfo{mnt: path.Join(DISK_MOUNT_ROOT, d.Name()), ds: DiskUsage(path.Join(DISK_MOUNT_ROOT, d.Name()))})
+			}
+		}
+	}
+
+	for filename, v := range Ssm.maptbl {
+		fmt.Println(filename, v)
+	}
+
+	fmt.Println("------------mount point capacity and sector allocate:")
+	for _, v := range Ssm.captbl {
+		fmt.Println(v.mnt, formatFileSize(v.ds.All), v.ds.MaxSector, v.ds.UsedSector)
+	}
+}
+
+func (Ssm *SSM) Allocate(sid string) (string, error) {
+	if v, ok := Ssm.regtbl[sid]; ok == true {
+		return v, nil
+	}
+
+	len := len(Ssm.captbl)
+	for i := 0; i < len; i++ {
+		di := Ssm.captbl[Ssm.cursor%len]
+		Ssm.cursor++
+		if di.ds.UsedSector < di.ds.MaxSector {
+			di.ds.UsedSector++
+			Ssm.regtbl[sid] = di.mnt
+
+			//
+			sids := Ssm.maptbl[di.mnt]
+			*sids = append(*sids, sid)
+
+			return di.mnt, nil
+		}
+	}
+
+	return "", errors.New("No suitable ssd available")
+}
+
+func (Ssm *SSM) Query(sid string) (string, error) {
+	if v, ok := Ssm.regtbl[sid]; ok == true {
+		return v, nil
+	}
+
+	return "", errors.New("can't find any ssd cached this sid")
+}
+
+func (Ssm *SSM) Delete(sid string) error {
+	mnt, ok := Ssm.regtbl[sid]
+	if ok == false {
+		return errors.New("this sector no mapped to any ssd before")
+	}
+
+	for _, v := range Ssm.captbl {
+		if v.mnt == mnt {
+			if v.ds.UsedSector > 0 {
+				v.ds.UsedSector--
+			}
+		}
+	}
+
+	delete(Ssm.regtbl, sid)
+	sids := (Ssm.maptbl[mnt])
+	len := len(*sids)
+	for i := 0; i < len; i++ {
+		if sid == (*sids)[i] {
+			*sids = append((*sids)[:i], (*sids)[i+1:]...)
+			break
+		}
+	}
+
+	return nil
+}
+
+func (Ssm *SSM) Show() error {
+	for mnt, sids := range Ssm.maptbl {
+		fmt.Println("mnt is:", mnt)
+		for _, sid := range *sids {
+			fmt.Println("    ", sid)
+		}
+	}
+
+	return nil
 }
--- cmd/lotus-hlm-worker/disk_pool_test.go
+++ cmd/lotus-hlm-worker/disk_pool_test.go
@@ -1,7 +1,33 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestDiskPool(t *testing.T) {
-	// TODO: testing
+	diskpool, _ := NewDiskPool()
+	for i := 0; i < 20; i++ {
+		//fmt.Println("s-t0%d", i)
+		sid := fmt.Sprintf("s-t0%d", i)
+		_, err := diskpool.Allocate(sid)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(sid)
+	}
+	fmt.Println("print before delete")
+	diskpool.Show()
+
+	for i := 3; i < 20; i += 2 {
+		sid := fmt.Sprintf("s-t0%d", i)
+		diskpool.Delete(sid)
+	}
+	fmt.Println("print after delete")
+	diskpool.Show()
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("Test begins....")
+	m.Run()
 }
