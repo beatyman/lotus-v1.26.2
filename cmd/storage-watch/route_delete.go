dd3afa25794bc3ad32bc70616a3a58ae81cddef0
zhoushuyue <zhouhsuyue@grandhelmsman.com>
2021-01-28 11:30:57 +0800

    support sectors for storage api

--- cmd/storage-watch/route_delete.go
+++ cmd/storage-watch/route_delete.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/gwaylib/errors"
 	"github.com/gwaylib/log"
@@ -14,16 +15,13 @@ func init() {
 }
 
 func deleteHandler(w http.ResponseWriter, r *http.Request) error {
-	sid := r.FormValue("sid")
-	if len(sid) == 0 {
+	sectors := r.FormValue("sectors")
+	if len(sectors) == 0 {
 		return writeMsg(w, 404, "sector not found")
 	}
+	sids := strings.Split(sectors, ",")
 
 	for _, repo := range repos {
-		cacheFile := filepath.Join(repo, "cache", sid)
-		sealedFile := filepath.Join(repo, "sealed", sid)
-		unsealedFile := filepath.Join(repo, "unsealed", sid)
-
 		func() {
 			// stop protect.
 			if err := chattrUnlock(filepath.Join(repo, "cache")); err != nil {
@@ -47,33 +45,37 @@ func deleteHandler(w http.ResponseWriter, r *http.Request) error {
 				}
 			}()
 
-			if err := os.RemoveAll(cacheFile); err != nil {
-				if !os.IsNotExist(err) {
-					log.Warn(errors.As(err))
-					return
-				}
-			} else {
-				log.Warnf("delete file:%s", cacheFile)
-			}
+			for _, sid := range sids {
+				cacheFile := filepath.Join(repo, "cache", sid)
+				sealedFile := filepath.Join(repo, "sealed", sid)
+				unsealedFile := filepath.Join(repo, "unsealed", sid)
 
-			if err := os.Remove(sealedFile); err != nil {
-				if !os.IsNotExist(err) {
-					log.Warn(errors.As(err))
-					return
+				if err := os.RemoveAll(cacheFile); err != nil {
+					if !os.IsNotExist(err) {
+						log.Warn(errors.As(err))
+						return
+					}
+				} else {
+					log.Warnf("delete file:%s", cacheFile)
 				}
-			} else {
-				log.Warnf("delete file:%s", sealedFile)
-			}
-			if err := os.Remove(unsealedFile); err != nil {
-				if !os.IsNotExist(err) {
-					log.Warn(errors.As(err))
-					return
+				if err := os.Remove(sealedFile); err != nil {
+					if !os.IsNotExist(err) {
+						log.Warn(errors.As(err))
+						return
+					}
+				} else {
+					log.Warnf("delete file:%s", sealedFile)
 				}
-			} else {
-				log.Warnf("delete file:%s", unsealedFile)
+				if err := os.Remove(unsealedFile); err != nil {
+					if !os.IsNotExist(err) {
+						log.Warn(errors.As(err))
+						return
+					}
+				} else {
+					log.Warnf("delete file:%s", unsealedFile)
+				}
 			}
 		}()
-
 	}
 	return writeMsg(w, 200, "OK")
 }
