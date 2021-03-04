d77587aca42058a89bac602c95393aacbb6a72fb
zhoushuyue <zhouhsuyue@grandhelmsman.com>
2021-03-04 20:31:06 +0800

    add dir view

--- cmd/lotus-monitor/main.go
+++ cmd/lotus-monitor/main.go
@@ -1,3 +1,21 @@
+// command
+// list nodes
+// curl -k -u hlm-miner:V4TitgRs0qJvWHwu https://127.0.0.1:8443/nodes
+//
+// ./miner.sh proving deadlines
+// curl -k -u hlm-miner:V4TitgRs0qJvWHwu https://127.0.0.1:8443/monitor?node=node-1&kind=wdpost
+//
+// ./miner.sh info
+// curl -k -u hlm-miner:V4TitgRs0qJvWHwu https://127.0.0.1:8443/monitor?node=node-1&kind=miner
+//
+// ./miner.sh proxy status --sync --mpool
+// curl -k -u hlm-miner:V4TitgRs0qJvWHwu https://127.0.0.1:8443/monitor?node=node-1&kind=proxy
+//
+// ./miner.sh actor control list
+// curl -k -u hlm-miner:V4TitgRs0qJvWHwu https://127.0.0.1:8443/monitor?node=node-1&kind=balance
+//
+// ./miner.sh hlm-storage status
+// curl -k -u hlm-miner:V4TitgRs0qJvWHwu https://127.0.0.1:8443/monitor?node=node-1&kind=hlm-storage
 package main
 
 import (
@@ -11,6 +29,8 @@ import (
 	"os/exec"
 	"path/filepath"
 	"time"
+
+	"github.com/filecoin-project/lotus/node/modules/auth"
 )
 
 var (
@@ -20,6 +40,26 @@ var (
 
 func main() {
 	flag.Parse()
+	http.HandleFunc("/nodes", func(w http.ResponseWriter, req *http.Request) {
+		username, passwd, ok := req.BasicAuth()
+		if !ok {
+			io.WriteString(w, "auth failed!\n")
+			return
+		}
+		if username != *usernameFlag || passwd != *passwdFlag {
+			io.WriteString(w, "auth failed, bye!\n")
+			return
+		}
+
+		dirs, err := ioutil.ReadDir("./node")
+		if err != nil {
+			io.WriteString(w, err.Error()+"\n")
+			return
+		}
+		for _, dir := range dirs {
+			io.WriteString(w, dir.Name()+"\n")
+		}
+	})
 
 	http.HandleFunc("/monitor", func(w http.ResponseWriter, req *http.Request) {
 		username, passwd, ok := req.BasicAuth()
@@ -31,60 +71,69 @@ func main() {
 			io.WriteString(w, "auth failed, bye!\n")
 			return
 		}
-
-		path := "./node"
-		dirs, err := ioutil.ReadDir(path)
-		if err != nil {
-			io.WriteString(w, err.Error())
+		node := req.FormValue("node")
+		if len(node) == 0 {
+			io.WriteString(w, "no node!\n")
 			return
 		}
-		for _, fs := range dirs {
-			// ignore the chain
-			if fs.Name() == "lotus" {
-				continue
-			}
-			filePath := filepath.Join(path, fs.Name(), ".lotusstorage")
-			if !fs.IsDir() {
-				continue
-			}
-			ctx, cancel := context.WithTimeout(context.TODO(), time.Minute)
-			defer cancel()
-			io.WriteString(w, fmt.Sprintf("\n\n==============%s begin==============\n", fs.Name()))
-			r0, err := exec.CommandContext(ctx, "./lotus-miner", "--repo", "./node/lotus", "--miner-repo", filePath, "proving", "deadlines").CombinedOutput()
+		kind := req.FormValue("kind")
+		if len(kind) == 0 {
+			io.WriteString(w, "no kind!\n")
+			return
+		}
+
+		path := "./node"
+		repo := filepath.Join(path, node, ".lotusstorage")
+		ctx, cancel := context.WithTimeout(context.TODO(), time.Minute)
+		defer cancel()
+		io.WriteString(w, fmt.Sprintf("\n\n==============%s begin==============\n", node))
+		defer io.WriteString(w, fmt.Sprintf("==============%s end==============\n", node))
+		switch kind {
+		case "wdpost":
+			r0, err := exec.CommandContext(ctx, "./lotus-miner", "--repo", "./node/lotus", "--miner-repo", repo, "proving", "deadlines").CombinedOutput()
 			if err != nil {
 				io.WriteString(w, err.Error()+"\n")
-				continue
+				return
 			}
 			w.Write(r0)
 
-			r2, err := exec.CommandContext(ctx, "./lotus-miner", "--repo", "./node/lotus", "--miner-repo", filePath, "info").CombinedOutput()
+		case "miner":
+			info, err := exec.CommandContext(ctx, "./lotus-miner", "--repo", "./node/lotus", "--miner-repo", repo, "info").CombinedOutput()
 			if err != nil {
 				io.WriteString(w, err.Error()+"\n")
-				continue
+				return
 			}
-			w.Write(r2)
-			r1, err := exec.CommandContext(ctx, "./lotus-miner", "--repo", "./node/lotus", "--miner-repo", filePath, "proxy", "status", "--mpool").CombinedOutput()
+			w.Write(info)
+		case "proxy":
+			proxy, err := exec.CommandContext(ctx, "./lotus-miner", "--repo", "./node/lotus", "--miner-repo", repo, "proxy", "status", "--sync", "--mpool").CombinedOutput()
 			if err != nil {
 				io.WriteString(w, err.Error()+"\n")
-				continue
+				return
 			}
-			w.Write(r1)
-			r3, err := exec.CommandContext(ctx, "./lotus-miner", "--repo", "./node/lotus", "--miner-repo", filePath, "actor", "control", "list").CombinedOutput()
+			w.Write(proxy)
+		case "balance":
+			r3, err := exec.CommandContext(ctx, "./lotus-miner", "--repo", "./node/lotus", "--miner-repo", repo, "actor", "control", "list").CombinedOutput()
 			if err != nil {
 				io.WriteString(w, err.Error()+"\n")
-				continue
+				return
 			}
 			w.Write(r3)
-			r4, err := exec.CommandContext(ctx, "./lotus-miner", "--repo", "./node/lotus", "--miner-repo", filePath, "hlm-storage", "status").CombinedOutput()
+		case "hlm-storage":
+			r4, err := exec.CommandContext(ctx, "./lotus-miner", "--repo", "./node/lotus", "--miner-repo", repo, "hlm-storage", "status").CombinedOutput()
 			if err != nil {
 				io.WriteString(w, err.Error()+"\n")
-				continue
+				return
 			}
 			w.Write(r4)
-			io.WriteString(w, fmt.Sprintf("==============%s end==============\n", fs.Name()))
+		default:
+			io.WriteString(w, "unknow kind\n")
+			return
 		}
 	})
 
+	if err := auth.CreateTLSCert("./lotus_crt.pem", "./lotus_key.pem"); err != nil {
+		log.Fatal(err)
+	}
 	// One can use generate_cert.go in crypto/tls to generate cert.pem and key.pem.
 	log.Printf("About to listen on 8443. Go to https://127.0.0.1:8443/")
 	err := http.ListenAndServeTLS(":8443", "lotus_crt.pem", "lotus_key.pem", nil)
