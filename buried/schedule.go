88a02ece4190eb62ba52be74d439cb1863d4a499
Acumes <295277302@qq.com>
2022-02-23 09:33:05 +0800

    去除日志输出

--- buried/schedule.go
+++ buried/schedule.go
@@ -123,7 +123,6 @@ func RunCollectWorkerInfo(cctx *cli.Context, timer int64, workerCfg ffiwrapper.W
 				_, span := spans.NewWorkerSpan(context.Background())
 				span.SetInfo(string(workerInfoStr))
 				span.End()
-				log.Info("================report======", workerInfo)
 				//				closer()
 			case <-quit:
 				ticker.Stop()
